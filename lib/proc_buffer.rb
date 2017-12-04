a9a17e2d8368cdc7df3d887f445bbfa9eb5ee8e3
LittleStevieBrule <stephendmcguckin@gmail.com>
2017-12-04 00:29:06 -0800

    fixed linter warnings

--- lib/proc_buffer.rb
+++ lib/proc_buffer.rb
@@ -1,26 +1,40 @@
 require 'proc_buffer/version'
 
+# TODO: Documentation
+# ProcBuffer
 class ProcBuffer
   include Enumerable
 
-  def initialize(*actions) ; @actions = actions end
+  def initialize(*actions)
+    @actions = actions
+  end
 
-  def actions ; @actions end
+  attr_reader :actions
 
-  def each ; actions.each { |action| yield action } end
+  def each
+    actions.each { |action| yield action }
+  end
 
-  def run ; map { |action| call(action) } end
+  def run
+    map { |action| call(action) }
+  end
 
-  def reverse_run ; actions.reverse.map { |action| call(action) } end
+  def reverse_run
+    actions.reverse.map { |action| call(action) }
+  end
 
-  def append(&block) ; actions << block end
+  def append(&block)
+    actions << block 
+  end
 
   alias push append
 
-  def call(obj) ; obj.respond_to?(:call) ? obj.call : obj end
+  def call(obj)
+    obj.respond_to?(:call) ? obj.call : obj
+  end
 
   def pop(n = nil)
-    pop = Proc.new do
+    pop = proc do
       actions.last.respond_to?(:call) ? actions.pop.call : actions.pop
     end
     return pop.call unless n
